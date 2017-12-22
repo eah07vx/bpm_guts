5e460772e26fdb8510c8572f8a70be524b35478a
akatre <akatre@salientprocess.com>
2017-12-22 13:05:27 -0600

    Fixed boolean class type reference

--- BPMIntegration/src/com/mck/crrb/AggregateAPIBeanInfo.java
+++ BPMIntegration/src/com/mck/crrb/AggregateAPIBeanInfo.java
@@ -25,15 +25,17 @@ public class AggregateAPIBeanInfo extends SimpleBeanInfo {
 			MethodDescriptor methodDescriptor1 = getMethodDescription(
 					"lookupInvoice",	//methodName
 					new String[] { "url (String)", "httpMethod (String)", "sslAlias (String)", "requestJSON (String)", "sopDebug (boolean)" }, // parameters/arguments names
-					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			Boolean.class }); // corresponding parameter types/classes
+					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			boolean.class }); // corresponding parameter types/classes
+			
 			MethodDescriptor methodDescriptor2 = getMethodDescription(
 					"simulatePrice",	//methodName
 					new String[] { "url (String)", "httpMethod (String)", "sslAlias (String)", "requestJSON (String)", "sopDebug (boolean)" }, // parameters/arguments names
-					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			Boolean.class }); // corresponding parameter types/classes
+					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			boolean.class }); // corresponding parameter types/classes
+			
 			MethodDescriptor methodDescriptor3 = getMethodDescription(
 					"submitCreditRebill",	//methodName
 					new String[] { "url (String)", "httpMethod (String)", "sslAlias (String)", "requestJSON (String)", "reqHeader (TWObject)", "sopDebug (boolean)" }, // parameters/arguments names
-					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			TWObject.class, 		Boolean.class }); // corresponding parameter types/classes
+					new Class[]  { String.class, 	String.class, 			String.class, 		String.class, 			TWObject.class, 		boolean.class }); // corresponding parameter types/classes
 
 			return new MethodDescriptor[] { methodDescriptor1, methodDescriptor2, methodDescriptor3 };
 		} catch (Exception e) {
@@ -52,7 +54,7 @@ public class AggregateAPIBeanInfo extends SimpleBeanInfo {
 			for (int i = 0; i < parameters.length; i++) {
 				ParameterDescriptor param = new ParameterDescriptor();
 				param.setShortDescription(parameters[i]);
-				param.setDisplayName(parameters[i].substring(0, parameters[i].indexOf(" ")));
+				param.setDisplayName(parameters[i]);
 				paramDescriptors[i] = param;
 			}
 			methodDescriptor = new MethodDescriptor(method, paramDescriptors);
@@ -60,5 +62,4 @@ public class AggregateAPIBeanInfo extends SimpleBeanInfo {
 
 		return methodDescriptor;
 	}
-
 }
